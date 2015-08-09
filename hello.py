cc219dd285c58f4526f862d5c9c3538a863c6d4e
Takahiro Sugiura <tksg2333@gmail.com>
2015-08-09 23:07:06 +0900

    update

--- hello.py
+++ hello.py
@@ -14,17 +14,18 @@ def sensePerson():
     return ans
 
 def main():
+    wd = "/home/pi/HomePi/"
     if sensePerson():
         now = datetime.now()
-        with open("speakerdata") as f:
+        with open(wd + "speakerdata") as f:
             prev = datetime.strptime(f.read(),"%c")
         dt = now - prev
         if dt.total_seconds() > 3600 * 6:
             if now.hour > 6 and now.hour < 10:
-                subprocess.call("aplay -D hw:1,0 /home/pi/HomePi/goodmorning.wav",shell=True)
+                subprocess.call("aplay -D hw:1,0 {0}goodmorning.wav".format(wd),shell=True)
             elif now.hour > 16:
-                subprocess.call("aplay -D hw:1,0 /home/pi/HomePi/comehome.wav",shell=True)
-        with open("speakerdata",'w') as f:
+                subprocess.call("aplay -D hw:1,0 {0}comehome.wav".format(wd),shell=True)
+        with open(wd + "speakerdata",'w') as f:
             f.write(now.strftime("%c"))
 
 if __name__ == '__main__':
